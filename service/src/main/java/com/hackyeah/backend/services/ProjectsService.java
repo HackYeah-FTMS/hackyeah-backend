4497a8c631df4b92cab6ec4b875223c818bd4f2a
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2020-11-27 23:15:05 +0100

    Add owner id, fix tags

--- service/src/main/java/com/hackyeah/backend/services/ProjectsService.java
+++ service/src/main/java/com/hackyeah/backend/services/ProjectsService.java
@@ -24,6 +24,7 @@ public class ProjectsService {
         return List.of(
                 new ProjectDto()
                         .id(1L)
+                        .ownerId(1L)
                         .title("Mój pomysł na liście")
                         .ideaImage("https://n-22-5.dcs.redcdn.pl/file/o2/redefine/cp/xj/xj1gx9v9k1kzz4x37wcagdhi6r4ckrwo.jpg")
                         .solutionImage("https://www.glebokaswiadomosc.pl/wp-content/uploads/2019/10/ekologiczne-konfetti.jpg")
@@ -33,6 +34,7 @@ public class ProjectsService {
                         .creationTimestamp(Timestamp.valueOf(LocalDateTime.now().minusDays(17)).getTime()),
                 new ProjectDto()
                         .id(2L)
+                        .ownerId(1L)
                         .title("Reużyj rolki!")
                         .ideaImage("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/paper-roll-on-orange-background-royalty-free-image-1584641876.jpg")
                         .solutionImage("https://i.pinimg.com/564x/54/d5/27/54d52748c025cc5c11aff93e9136ba31.jpg")
@@ -42,11 +44,12 @@ public class ProjectsService {
                         .creationTimestamp(Timestamp.valueOf(LocalDateTime.now().minusDays(7)).getTime()),
                 new ProjectDto()
                         .id(3L)
+                        .ownerId(2L)
                         .title("Plecaczek")
                         .ideaImage("https://img.buzzfeed.com/buzzfeed-static/static/2019-04/11/15/asset/buzzfeed-prod-web-01/sub-buzz-19419-1555009752-1.jpg")
                         .solutionImage("https://img.buzzfeed.com/buzzfeed-static/static/2019-04/11/15/asset/buzzfeed-prod-web-01/sub-buzz-19661-1555009856-1.jpg")
                         .description("Bardzo prosty sposób na stworzenie swojego plecaka")
-                        .tags(List.of("rolka", "papier"))
+                        .tags(List.of("plecak", "włóczka"))
                         .points(14)
                         .creationTimestamp(Timestamp.valueOf(LocalDateTime.now()).getTime())
         );
